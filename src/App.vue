6d423dc7b1c8eba00d118ae8e2fb442a79214c8e
Andrew Prosvetkin <aprosvetkin.exceedteam@gmail.com>
2022-04-22 21:07:29 +0300

    add redirect

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-@Component({})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  private mounted() {
+    if (!this.$route.name) {
+      this.$router.push({ name: "login" });
+    }
+  }
+}
 </script>
